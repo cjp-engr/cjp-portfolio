0335b39a713fb23a6669251a739514c0efcca200
cjp-engr <67772382+cjp-engr@users.noreply.github.com>
2022-05-11 18:12:18 -0700

    first commit

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,19 +1,24 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rajshekhar26.github.io/cleanfolio',
+  homepage: 'https://cjp-engr.github.io/portfolio',
   title: 'JS.',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Carmen Joy Palsario',
+  role: 'Aspiring Software Developer | Software QA Engineer',
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
+    'Hi! I am currently working as a Software QA Engineer and looking forward' +
+    ' on becoming part of the Software Development team. ' + 
+    'Building applications and fixing bugs thrills me a lot. ' +
+    'I am knowledgeable in Flutter and BloC as my state management solution.'+
+    '\n\n'+
+    'I am the type of person who wants to explore and learn anything about technologies. ',
+  resume: 'https://drive.google.com/file/d/14Eka0Myg5b_n5mRhbZK3ASTpoIrm9AoI/view?usp=sharing',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/carmenjoy/',
+    github: 'https://github.com/cjp-engr',
   },
 }
 
@@ -21,26 +26,26 @@ const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'Project 1',
+    name: 'Todo Application',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
+      'The app can help increase productivity, remember important things to do and prioritize tasks effectively.',
+    stack: ['Flutter', 'Firebase'],
+    sourceCode: 'https://github.com/cjp-engr/cj-flutter-todo-app-w-firebase',
     livePreview: 'https://github.com',
   },
   {
-    name: 'Project 2',
+    name: 'News Application',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
+      'The app can display the latest news by category and search. The user can also check the current weather condition by searching the city. ',
+    stack: ['Flutter', 'Firebase'],
+    sourceCode: 'https://github.com/cjp-engr/cj-flutter-news-app-w-firebase',
     livePreview: 'https://github.com',
   },
   {
-    name: 'Project 3',
+    name: 'Employee Management System Application',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
+      'Still in progress',
+    stack: ['Flutter', 'MongoDB', 'NodeJS', 'ExpressJS'],
     sourceCode: 'https://github.com',
     livePreview: 'https://github.com',
   },
@@ -51,21 +56,19 @@ const skills = [
   // if there are no skills, Skills section won't show up
   'HTML',
   'CSS',
-  'JavaScript',
-  'TypeScript',
-  'React',
-  'Redux',
-  'SASS',
-  'Material UI',
+  'Java',
+  'Flutter',
+  'Python',
+  'MongoDB',
+  'Firebase',
   'Git',
   'CI/CD',
-  'Jest',
-  'Enzyme',
+  'Rest API',
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'engr.carmenjoy14@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
