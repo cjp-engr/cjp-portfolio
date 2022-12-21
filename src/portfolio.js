5665b59f4203e9a0b403ccde12e323669fdf9be8
carmenjoy <67772382+cjp-engr@users.noreply.github.com>
2022-12-21 19:01:01 +0800

    update portfolio

--- src/portfolio.js
+++ src/portfolio.js
@@ -7,17 +7,15 @@ const header = {
 const about = {
   // all the properties are optional - can be left empty or deleted
   name: 'Carmen Joy Palsario',
-  role: 'Aspiring Software Developer | Software QA Engineer',
+  role: 'Flutter Developer | Software QA Engineer',
   description:
-    'Hi! I am currently working as a Software QA Engineer and looking forward' +
-    ' on becoming part of the Software Development team. ' + 
-    'Building applications and fixing bugs thrills me a lot. ' +
-    'I am knowledgeable in Flutter and BloC as my state management solution.'+
-    '\n\n'+
-    'I am the type of person who wants to explore and learn anything about technologies. ',
-  resume: 'https://drive.google.com/file/d/1PYlWiFksOZpBOEoA9AN7AlgZavgBZhQG/view?usp=sharing',
+    'As a Flutter Developer and Software QA Engineer, I have a strong knowledge in software development ' +
+    'and a passion for delivering high-quality products. My experience with Flutter has allowed me to ' +
+    'create cross-platform mobile applications with a native feel, and my skills in software QA have equipped me ' +
+    'with the ability to thoroughly test and debug software to ensure it meets all requirements and performs optimally.',
+  resume: 'https://drive.google.com/file/d/1IJ8c0mJgD5Z9O6wPz_yebnYmov7bNmna/view?usp=sharing',
   social: {
-    linkedin: 'https://www.linkedin.com/in/carmenjoy/',
+    linkedin: 'https://www.linkedin.com/in/carmen9696/',
     github: 'https://github.com/cjp-engr',
   },
 }
@@ -28,7 +26,7 @@ const projects = [
   {
     name: 'Todo Application',
     description:
-      'The app can help increase productivity, remember important things to do and prioritize tasks effectively.',
+      'It can help increase productivity, remember important things to do and prioritize tasks effectively.',
     stack: ['Flutter', 'Firebase'],
     sourceCode: 'https://github.com/cjp-engr/cj-flutter-todo-app-w-firebase',
     livePreview: 'https://github.com',
@@ -36,17 +34,36 @@ const projects = [
   {
     name: 'News Application',
     description:
-      'The app can display the latest news by category and search. The user can also check the current weather condition by searching the city. ',
+      'It can display the latest news by category and search. The user can also check the current weather condition by searching the city. ',
     stack: ['Flutter', 'Firebase'],
     sourceCode: 'https://github.com/cjp-engr/cj-flutter-news-app-w-firebase',
     livePreview: 'https://github.com',
   },
   {
-    name: 'Employee Management System Application',
+    name: 'Employee Mgmt. System Application',
     description:
-      'Still in progress',
+      'It allows employees to easily clock in and out of their shifts. They can also view their ' +
+      'attendance records and salary information in real-time, helping them to stay organized and informed.',
     stack: ['Flutter', 'MongoDB', 'NodeJS', 'ExpressJS'],
-    sourceCode: 'https://github.com',
+    sourceCode: 'https://github.com/cjp-engr/cj-flutter-ems-w-mongoDB',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Automated Sauce Demo Application using Cypress.io',
+    description:
+      'The automated shopping application utilizes the Cypress automation framework to streamline ' +
+      'the online shopping experience.',
+    stack: ['Cypress', 'TypeScript'],
+    sourceCode: 'https://github.com/cjp-engr/cj-cypress-swag-labs',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Automated Online Shopping Application using Cypress.io',
+    description:
+      'The Cypress framework allows for fast and reliable testing of the application, ensuring ' +
+      'that it is functioning properly and providing a seamless experience for real users.',
+    stack: ['Cypress', 'TypeScript'],
+    sourceCode: 'https://github.com/cjp-engr/cj-cypress-shopping-app',
     livePreview: 'https://github.com',
   },
 ]
@@ -57,18 +74,21 @@ const skills = [
   'HTML',
   'CSS',
   'Java',
-  'Flutter',
+  'Flutter & Dart',
   'Python',
   'MongoDB',
   'Firebase',
   'Git',
   'CI/CD',
   'Rest API',
+  'Cypress',
+  'JavaScript',
+  'TypeScript'
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'engr.carmenjoy14@gmail.com',
+  email: 'carmenjoyp96@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
