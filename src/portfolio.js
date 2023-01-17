ae9f8f2ef4d690cd2a736742b0b12daecb8b1aec
carmenjoy <67772382+cjp-engr@users.noreply.github.com>
2023-01-17 08:37:10 +0800

    updated portfolio

--- src/portfolio.js
+++ src/portfolio.js
@@ -13,7 +13,7 @@ const about = {
     'and a passion for delivering high-quality products. My experience with Flutter has allowed me to ' +
     'create cross-platform mobile applications with a native feel, and my skills in software QA have equipped me ' +
     'with the ability to thoroughly test and debug software to ensure it meets all requirements and performs optimally.',
-  resume: 'https://drive.google.com/file/d/1royKfHce-lN1qT_4UnBuw38Q57k-64zk/view?usp=sharing',
+  resume: 'https://drive.google.com/file/d/1royKfHce-lN1qT_4UnBuw38Q57k-64zk/view?usp=share_link',
   social: {
     linkedin: 'https://www.linkedin.com/in/carmen9696/',
     github: 'https://github.com/cjp-engr',
@@ -27,43 +27,48 @@ const projects = [
     name: 'Todo Application',
     description:
       'It helps increase productivity, remember important things to do and prioritize tasks effectively.',
-    stack: ['Flutter', 'Firebase'],
+    stack: ['Flutter', 'Firebase', 'BLoC'],
     sourceCode: 'https://github.com/cjp-engr/cj-flutter-todo-app-w-firebase',
     livePreview: 'https://github.com',
   },
   {
     name: 'News Application',
     description:
-      'It displays the latest news by category and search. The user can also check the current weather condition by searching the city. ',
-    stack: ['Flutter', 'Firebase'],
+      'It displays the latest news by category or by search. The user can also check the current weather condition by searching the city. ',
+    stack: ['Flutter', 'Firebase', 'BLoC'],
     sourceCode: 'https://github.com/cjp-engr/cj-flutter-news-app-w-firebase',
     livePreview: 'https://github.com',
   },
   {
     name: 'Employee Mgmt. System Application',
     description:
-      'It allows employees to easily clock in and out of their shifts. They can also view their ' +
-      'attendance records and salary information in real-time, helping them to stay organized and informed.',
-    stack: ['Flutter', 'MongoDB', 'NodeJS', 'ExpressJS'],
+      'It allows employee to perform variety of tasks such as clocking in and clocking out, check the attendance and number of hours work per day.',
+    stack: ['Flutter', 'MongoDB', 'NodeJS', 'ExpressJS', 'BLoC'],
     sourceCode: 'https://github.com/cjp-engr/cj-flutter-ems-w-mongoDB',
     livePreview: 'https://github.com',
   },
   {
-    name: 'Automated Sauce Demo Application using Cypress.io',
+    name: 'First Automated Online Shopping Application using Cypress.io',
     description:
-      'The automated shopping application utilizes the Cypress automation framework to streamline ' +
-      'the online shopping experience.',
+      'Used Cypress.io framework to automate the test scripts. It refreshed my knowledge on using the tool.',
     stack: ['Cypress', 'TypeScript'],
     sourceCode: 'https://github.com/cjp-engr/cj-cypress-swag-labs',
     livePreview: 'https://github.com',
   },
   {
-    name: 'Automated Online Shopping Application using Cypress.io',
+    name: 'Second Automated Online Shopping Application using Cypress.io',
     description:
-      'The Cypress framework allows for fast and reliable testing of the application, ensuring ' +
-      'that it is functioning properly and providing a seamless experience for real users.',
+      'Used Cypress.io framework and TypeScript as a programming language to automate the test scripts.',
     stack: ['Cypress', 'TypeScript'],
-    sourceCode: 'https://github.com/cjp-engr/cj-cypress-shopping-app',
+    sourceCode: 'https://github.com/cjp-engr/cj-cypress-askomdch',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Todo Application with Clean Architecture',
+    description:
+      'Coming soon. This application implementes the clean architecture. Clean architecture has several advantages including separation of concerns and modularity.',
+    stack: ['Flutter', 'Firebase', 'Dependency Injection', 'BLoC'],
+    sourceCode: 'https://github.com/cjp-engr/cj-flutter-todo-clean-architecture',
     livePreview: 'https://github.com',
   },
 ]
